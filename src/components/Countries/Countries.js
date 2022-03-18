2c09f36ccc4b69bc1d541a9eee79a3a1690df214
Debashish D. Dev <hello@debashish.dev>
2022-03-18 18:27:58 +0600

    Showing Countris from other  folder

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countris in other file</h2>
+        </div>
+    );
+};
+
+export default Countries;
