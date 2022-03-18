549dbb105b5b8f78f1303141913f2b4cd66a594b
Debashish D. Dev <hello@debashish.dev>
2022-03-18 18:06:31 +0600

    Removed Old components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+
     </div>
   );
-}
-
-function LoadCountries(){
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then( data => setCountries(data));
-  }, [])
-  return(
-    <div>
-      <h1>Visit Country</h1>
-      <h3>Countries Num: {countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common}></Country>)
-      }
-    </div>
-  )
-}
-
-function Country(props){
-  return (
-    <div>
-      <h2>{props.name}</h2>
-    </div>
-  )
 }
 
 export default App;
