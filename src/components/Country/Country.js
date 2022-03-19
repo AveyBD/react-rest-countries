b0668c8aa0d91d2c904fb9fcfb45ad45d07046c8
Debashish D. Dev <hello@debashish.dev>
2022-03-19 14:52:34 +0600

    Destrcturing

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    const{name, flags, population, region} = props;
     return (
         <div className='country'>
-            <img src={props.flags.png} alt={props.name.common} srcSet="" />
-            <h2>{props.name.common}</h2>
-            <p>Population: {props.population}</p>
+            <img src={flags.png} alt={name.common} srcSet="" />
+            <h2>{name.common}</h2>
+            <p>Population: {population}</p>
+            <p><small>Region: {region}</small></p>
             {console.log(props)}
         </div>
     );
